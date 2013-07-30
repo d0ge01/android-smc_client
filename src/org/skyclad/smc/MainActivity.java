8f8489479c3cb109f72320264169e022c4961ee6
d0ge01 <not.d0ge@anche.no>
2013-07-30 15:14:32 +0200

    first commit

---
+++ src/org/skyclad/smc/MainActivity.java
@@ -0,0 +1,26 @@
+package org.skyclad.smc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void connect(View view) {
+		// Do something
+	}
+}
